c5d606d6ba67366e322c812ac6bb3b7774410d8f
Sergio Santos <sergiosantos@wizsolucoes.com.br>
2023-01-18 14:51:02 -0300

    andamento

---
+++ GeekShopping/GeekShopping.IdentityServer/Configuration/IdentityConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace GeekShopping.IdentityServer.Configuration
+{
+    public static class IdentityConfiguration
+    {
+        public const string Admin = "Admin";
+        public const string Customer = "Customer";
+    }
+}
